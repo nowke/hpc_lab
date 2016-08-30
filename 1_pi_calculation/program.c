c08e2b27363d49cfa4e5df8fc3bd9ad242b024dd
Navaneesh Kumar <navaneeshkumarb@gmail.com>
2016-08-30 23:18:13 +0530

    Added Program: PI Calculation

--- 1_pi_calculation/program.c
+++ 1_pi_calculation/program.c
@@ -1,28 +1,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#include <string.h>
-#include<omp.h>
+#include <omp.h>
 
 #define PI 3.1415926538837211
 
 int main(int argc, char* argv[]) {
-  double pi, x, y, z;
-  int intervals, i, count=0;
+    int i, intervals;
+    double x, totalsum, h;
 
-  printf("Enter no. of intervals: ");
-  scanf("%d", &intervals);
+    intervals = atoi(argv[1]);
+    
+    omp_set_num_threads(atoi(argv[2]));
 
-  #pragma omp parallel for
-  for (i=0; i<intervals; i++) {
-    x = (double)rand()/RAND_MAX;
-		y = (double)rand()/RAND_MAX;
-		z = x*x + y*y;
-		if (z <= 1) count++;
-  }
-  pi=(double)count/ intervals * 4;
+    totalsum = 0.0;
+    h = 1.0 / intervals;
+    double t1 = omp_get_wtime();
+    
+    #pragma omp parallel for private(x) reduction(+:totalsum)
+    for (i=1; i<intervals; i++) {
+        x = h * (i+0.5);
+        totalsum =totalsum + 4.0 / (1.0 + x * x);
+    }
 
-  printf("\nAcutal PI: %1.15lf\n", PI);
-  printf("Calculated PI: %1.15lf\n", pi);
-  printf("Error: %1.15lf\n", fabs(pi - PI));
-}
+    totalsum = totalsum * h;
+    double t2 = omp_get_wtime();
+
+
+    printf("\nActual PI: %1.15lf\n", PI);
+    printf("Calculated PI: %1.15lf\n", totalsum);
+    printf("Error: %1.15lf\n", fabs(totalsum - PI));
+
+    printf("Time taken: %lf ms\n", (t2 - t1)*1000);
+    return 0;
+}   
