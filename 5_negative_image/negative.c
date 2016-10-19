4ac28142f3ef0a3e7d14c4ba78b4a37e0a00a815
Navaneesh Kumar <navaneeshkumarb@gmail.com>
2016-10-19 10:20:28 +0530

    Tabs -> Spaces

--- 5_negative_image/negative.c
+++ 5_negative_image/negative.c
@@ -5,56 +5,56 @@
 
 int main(int argc, char *argv[]) {
 
-	if (argc < 4) {
-		printf("Usage: ./negative input.png output.png num_threads\n");
-		return 1;
-	}
+    if (argc < 4) {
+        printf("Usage: ./negative input.png output.png num_threads\n");
+        return 1;
+    }
 
-  	char *input_file = argv[1];
-  	char *output_file = argv[2];
-  	int num_threads = atoi(argv[3]);
+    char *input_file = argv[1];
+    char *output_file = argv[2];
+    int num_threads = atoi(argv[3]);
 
-	int color, x, y, i;
-	int red, green, blue;
+    int color, x, y, i;
+    int red, green, blue;
 
-	FILE *fp;
+    FILE *fp;
 
-	if((fp = fopen(input_file, "r")) == NULL) {
-		printf("Error opening file %s\n", input_file);
-		return 1;
-	}
+    if((fp = fopen(input_file, "r")) == NULL) {
+        printf("Error opening file %s\n", input_file);
+        return 1;
+    }
 
-	gdImagePtr img = gdImageCreateFromPng(fp);
-	int width = gdImageSX(img);
-	int height = gdImageSY(img);
+    gdImagePtr img = gdImageCreateFromPng(fp);
+    int width = gdImageSX(img);
+    int height = gdImageSY(img);
 
-	double t1 = omp_get_wtime();
+    double t1 = omp_get_wtime();
 
-	#pragma omp parallel for private(y, color, red, green, blue) num_threads(num_threads)
-	for(x=0; x<width; x++) {
-		for(y=0; y<height; y++) {
-		    color = x + 0;
-		    color = gdImageGetPixel(img, x, y);
-		    red   = 255 - gdImageRed(img, color);
-		    green = 255 - gdImageGreen(img, color);
-		    blue  = 255 - gdImageBlue(img, color);
-		    color = gdImageColorAllocate(img, red, green, blue);
-		    gdImageSetPixel(img, x, y, color);
-		}
-	}
+    #pragma omp parallel for private(y, color, red, green, blue) num_threads(num_threads)
+    for(x=0; x<width; x++) {
+        for(y=0; y<height; y++) {
+            color = x + 0;
+            color = gdImageGetPixel(img, x, y);
+            red   = 255 - gdImageRed(img, color);
+            green = 255 - gdImageGreen(img, color);
+            blue  = 255 - gdImageBlue(img, color);
+            color = gdImageColorAllocate(img, red, green, blue);
+            gdImageSetPixel(img, x, y, color);
+        }
+    }
 
-	double t2 = omp_get_wtime();
+    double t2 = omp_get_wtime();
 
-	if((fp=fopen(output_file, "w")) == NULL) {
-		printf("Error opening output file %s\n", output_file);
-		return 1;
-	}
+    if((fp=fopen(output_file, "w")) == NULL) {
+        printf("Error opening output file %s\n", output_file);
+        return 1;
+    }
 
-	gdImagePng(img, fp);
-	gdImageDestroy(img);
-	fclose(fp);
+    gdImagePng(img, fp);
+    gdImageDestroy(img);
+    fclose(fp);
 
-	printf("File Size: %dx%d\n", width, height);
-	printf("Time Taken: %.3lfms\n",(t2 - t1) * 1000);
-	return 0;
+    printf("File Size: %dx%d\n", width, height);
+    printf("Time Taken: %.3lfms\n",(t2 - t1) * 1000);
+    return 0;
 }
