1733e89355f211cf913bcda4192d5224fa71ec05
Navaneesh Kumar <navaneeshkumarb@gmail.com>
2016-10-22 00:11:30 +0530

    Fix Indentation

--- 6_multitasking/multitask.cpp
+++ 6_multitasking/multitask.cpp
@@ -8,12 +8,12 @@ using namespace std;
 int num_threads, upper_bound;
 
 bool isPrime(int num) {
-	for (int i=2; i<num; i++) {
-		if (num % i == 0) {
-			return false;
+    for (int i=2; i<num; i++) {
+        if (num % i == 0) {
+            return false;
         }
-	}
-	return true;
+    }
+    return true;
 }
 
 /**
